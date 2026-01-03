f801d5629c76a159ba64e36229026125ab655013
natsuki <natsuki.uji410@gmail.com>
2026-01-03 18:29:10 +0900

    backend-first-commit

---
+++ backend/pochitto-calendar-backend/pochitto-calendar-backend/src/main/java/com/pochitto/backend/PochittoCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pochitto.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PochittoCalendarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PochittoCalendarBackendApplication.class, args);
+	}
+
+}
---
+++ backend/pochitto-calendar-backend/pochitto-calendar-backend/src/test/java/com/pochitto/backend/PochittoCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pochitto.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochittoCalendarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
